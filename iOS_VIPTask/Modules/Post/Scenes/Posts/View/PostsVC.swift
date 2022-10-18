eb20c69debfa62c565cb85cfb2b9401fadd65a95
Abd2lrady <abd2lrady@gmail.com>
2022-10-18 11:26:18 +0200

    Adding networkClientusing AlamoFire, Making APIRouter for diffrent request parts generic based on APIRouter Protocol

--- iOS_VIPTask/Modules/Post/Scenes/Posts/View/PostsVC.swift
+++ iOS_VIPTask/Modules/Post/Scenes/Posts/View/PostsVC.swift
@@ -12,6 +12,9 @@ class PostsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        APIClient.shared.request(for: .getPosts) { result in
+            print(result)
+        }
     }
 
 
---
+++ iOS_VIPTask/Resources/Constants/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  iOS_VIPTask
+//
+//  Created by Fintech on 17/10/2022.
+//
+
+import Foundation
+
+enum Constants {
+    
+}
+
+extension Constants {
+    enum XIPFileNames: String {
+        case PostsVC = "PostsVC"
+    }
+}
+
+extension Constants {
+    enum API {
+        enum baseURLs {
+            static let Posts = "https://dummyjson.com"
+        }
+    }
+}
